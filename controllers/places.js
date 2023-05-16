a8bfffd860b3389f523a6539ab34da1561248ede
Christoph Nientied <christophnientied@googlemail.com>
2023-05-16 18:19:19 +0200

    connected SQL to code

--- controllers/places.js
+++ controllers/places.js
@@ -1,13 +1,35 @@
+const pool = require("../db");
 
-const getAllPlaces = (req, res) => {
-    res.send(`<h2>Get all Places!</h2>`)
-}
+const getAllPlaces = async (req, res) => {
+  try {
+    const queryResult = await pool.query("SELECT * FROM places;");
+    const allPlaces = queryResult.rows;
+    res.status(200).json(allPlaces);
+    console.log("log all places:", allPlacesResult);
+  } catch (err) {
+    console.error(err);
+    res.status(404).send("Something is not working:", err);
+  }
+};
 
-const getPlaceByID = (req, res) => {
-    console.log(req);
-    const placeId = req.params.id
-    console.log("PlaceId aus Params", placeId);
-    res.send(`<h2>Get one Place By Id: ${placeId}</h2>`)
-}
+const getPlaceByID = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  try {
+    const queryResult = await pool.query("SELECT * FROM places WHERE id=$1", [
+      id,
+    ]);
+    if (queryResult.rowCount !== 0) {
+      const [singlePlace] = queryResult.rows;
+      console.log(queryResult.rowCount);
+      res.status(200).json(singlePlace);
+    } else {
+      res.status(404).send(`Place with id ${id} not found`);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(404).send(`Something is not working: ${err}`);
+  }
+};
 
-module.exports = {getAllPlaces, getPlaceByID};
+module.exports = { getAllPlaces, getPlaceByID };
